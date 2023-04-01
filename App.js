215153055a63af96bcc7e4c11b5ebf85a646680e
malekqs <malek.kbh@gmail.com>
2023-04-01 15:26:01 +0300

    card component

--- App.js
+++ App.js
@@ -1,23 +1,66 @@
 import React from 'react';
-import {View} from 'react-native';
-import MainNavigator from './route/nav';
-import TestProvider from './store/testProvider';
-import TestContext from './store/testContext';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import Card from './src/components/card';
 
 const App = () => {
+  const data = [
+    'btata',
+    'roz',
+    'batngan',
+    'shawerma',
+    'stick',
+    'mansaf',
+    'pizza',
+    'frika',
+    'water',
+    'cola',
+    'tamer',
+    'ktaeyef',
+  ];
+
+  const renderCards = () => {
+    const cards = data.map(name => <Card title={name} />);
+    return cards;
+  };
+
   return (
-    <View style={{flex:1 , backgroundColor:'green'}}>
-      <TestProvider>
-        <TestContext.Consumer>
-          {context => (
-            <View style={{flex: 1, backgroundColor: 'red'}}>
-              <MainNavigator />
-            </View>
-          )}
-        </TestContext.Consumer>
-      </TestProvider>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {renderCards()}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    // borderWidth: 4,
+    marginTop: 10,
+    backgroundColor: 'green',
+    height: 300,
+    width: 300,
+    borderRadius: 20,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    // borderWidth:1 ,
+  },
+  text2: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'red',
+    // borderWidth:1
+  },
+});
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,35 +1,38 @@
-import React, {useState} from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Card = props => {
-  var userId = props.id || 0;
-
-  const [id, setId] = useState(userId);
-
-  const onCardsPress = () => {
-    setId(id + 1);
-    console.log('id: ', userId);
-    //triger
-    props.onCardChange?.(props.text);
-  };
 
   return (
-    <Pressable onPress={onCardsPress}>
-      <View style={styles.cardContainer}>
-        <Text>{props.text || '404'}</Text>
-        <Text>{id}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.card}>
+      <Text style={styles.text}>{props.title}</Text>
+      <Text style={styles.text2}>!!!</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  cardContainer: {
-    width: '60%',
-    height: 100,
-    borderWidth: 1,
-    alignSelf: 'center',
-    marginBottom: 10,
+  card: {
+    // borderWidth: 4,
+    marginTop: 10,
+    backgroundColor: 'green',
+    height: 300,
+    width: 300,
+    borderRadius: 20,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    // borderWidth:1 ,
+  },
+  text2: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'red',
+    // borderWidth:1
   },
 });
 
